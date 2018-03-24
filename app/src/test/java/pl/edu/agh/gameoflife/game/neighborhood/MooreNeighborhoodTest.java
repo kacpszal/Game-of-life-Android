f175c15bddb8c4a0b359b763336221f7b0a6d92a
Kacper Szalwa <kacper511@gmail.com>
2018-03-25 00:15:55 +0100

    Small refactoring in tests (unused import and fixed names).

--- app/src/test/java/pl/edu/agh/gameoflife/game/neighborhood/MooreNeighborhoodTest.java
+++ app/src/test/java/pl/edu/agh/gameoflife/game/neighborhood/MooreNeighborhoodTest.java
@@ -1,4 +1,4 @@
-package pl.edu.agh.gameoflife.game.Neighborhood;
+package pl.edu.agh.gameoflife.game.neighborhood;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -8,18 +8,11 @@ import pl.edu.agh.gameoflife.game.cell.CellFactory;
 import pl.edu.agh.gameoflife.game.cell.SimpleCell;
 import pl.edu.agh.gameoflife.game.cell.SimpleCellFactory;
 import pl.edu.agh.gameoflife.game.grid.EndlessGrid;
-import pl.edu.agh.gameoflife.game.neighborhood.AbstractCellNeighborhood;
-import pl.edu.agh.gameoflife.game.neighborhood.MooreNeighborhood;
-import pl.edu.agh.gameoflife.game.neighborhood.VonNeumannNeighborhood;
 import pl.edu.agh.gameoflife.test.RobolectricTest;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
-/**
- * Test for VonNeumann Neighborhood
- */
-
-public class VonNeumannTest extends RobolectricTest {
+public class MooreNeighborhoodTest extends RobolectricTest {
     EndlessGrid<SimpleCell> grid;
     CellFactory<SimpleCell> cellFactory;
     AbstractCellNeighborhood neighborhood;
@@ -28,34 +21,36 @@ public class VonNeumannTest extends RobolectricTest {
     public void setup() {
         cellFactory = new SimpleCellFactory();
         grid = new EndlessGrid<>(10, 10, cellFactory);
-
     }
 
     @Test
     public void notifyNeighborsSmallRadius() {
-        neighborhood = new VonNeumannNeighborhood(grid, 1);
+        neighborhood = new MooreNeighborhood(grid, 1);
         Cell cell = new SimpleCell(2, 2, 1);
+        grid.putCell(cellFactory.create(1, 1));
         grid.putCell(cellFactory.create(1, 2));
+        grid.putCell(cellFactory.create(1, 3));
         grid.putCell(cellFactory.create(2, 1));
         grid.putCell(cellFactory.create(2, 3));
+        grid.putCell(cellFactory.create(3, 1));
         grid.putCell(cellFactory.create(3, 2));
+        grid.putCell(cellFactory.create(3, 3));
 
         neighborhood.notifyNeighbors(cell);
 
+        assertThat(grid.getCell(1, 1).getNeighborCount()).isEqualTo(1);
         assertThat(grid.getCell(1, 2).getNeighborCount()).isEqualTo(1);
+        assertThat(grid.getCell(1, 3).getNeighborCount()).isEqualTo(1);
         assertThat(grid.getCell(2, 1).getNeighborCount()).isEqualTo(1);
         assertThat(grid.getCell(2, 3).getNeighborCount()).isEqualTo(1);
+        assertThat(grid.getCell(3, 1).getNeighborCount()).isEqualTo(1);
         assertThat(grid.getCell(3, 2).getNeighborCount()).isEqualTo(1);
-
-        assertThat(grid.getCell(1, 1).getNeighborCount()).isEqualTo(0);
-        assertThat(grid.getCell(3, 3).getNeighborCount()).isEqualTo(0);
-        assertThat(grid.getCell(1, 3).getNeighborCount()).isEqualTo(0);
-        assertThat(grid.getCell(3, 1).getNeighborCount()).isEqualTo(0);
+        assertThat(grid.getCell(3, 3).getNeighborCount()).isEqualTo(1);
     }
 
     @Test
     public void notifyNeighborsBiggerRadius() {
-        neighborhood = new VonNeumannNeighborhood(grid, 3);
+        neighborhood = new MooreNeighborhood(grid, 3);
         Cell cell = new SimpleCell(5, 5, 1);
 
         grid.putCell(cellFactory.create(2, 5));
--- app/src/test/java/pl/edu/agh/gameoflife/game/neighborhood/VonNeumannNeighborhoodTest.java
+++ app/src/test/java/pl/edu/agh/gameoflife/game/neighborhood/VonNeumannNeighborhoodTest.java
@@ -1,4 +1,4 @@
-package pl.edu.agh.gameoflife.game.Neighborhood;
+package pl.edu.agh.gameoflife.game.neighborhood;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -8,13 +8,11 @@ import pl.edu.agh.gameoflife.game.cell.CellFactory;
 import pl.edu.agh.gameoflife.game.cell.SimpleCell;
 import pl.edu.agh.gameoflife.game.cell.SimpleCellFactory;
 import pl.edu.agh.gameoflife.game.grid.EndlessGrid;
-import pl.edu.agh.gameoflife.game.neighborhood.AbstractCellNeighborhood;
-import pl.edu.agh.gameoflife.game.neighborhood.MooreNeighborhood;
 import pl.edu.agh.gameoflife.test.RobolectricTest;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
-public class MoorNeighborhoodTest extends RobolectricTest {
+public class VonNeumannNeighborhoodTest extends RobolectricTest {
     EndlessGrid<SimpleCell> grid;
     CellFactory<SimpleCell> cellFactory;
     AbstractCellNeighborhood neighborhood;
@@ -23,36 +21,34 @@ public class MoorNeighborhoodTest extends RobolectricTest {
     public void setup() {
         cellFactory = new SimpleCellFactory();
         grid = new EndlessGrid<>(10, 10, cellFactory);
+
     }
 
     @Test
     public void notifyNeighborsSmallRadius() {
-        neighborhood = new MooreNeighborhood(grid, 1);
+        neighborhood = new VonNeumannNeighborhood(grid, 1);
         Cell cell = new SimpleCell(2, 2, 1);
-        grid.putCell(cellFactory.create(1, 1));
         grid.putCell(cellFactory.create(1, 2));
-        grid.putCell(cellFactory.create(1, 3));
         grid.putCell(cellFactory.create(2, 1));
         grid.putCell(cellFactory.create(2, 3));
-        grid.putCell(cellFactory.create(3, 1));
         grid.putCell(cellFactory.create(3, 2));
-        grid.putCell(cellFactory.create(3, 3));
 
         neighborhood.notifyNeighbors(cell);
 
-        assertThat(grid.getCell(1, 1).getNeighborCount()).isEqualTo(1);
         assertThat(grid.getCell(1, 2).getNeighborCount()).isEqualTo(1);
-        assertThat(grid.getCell(1, 3).getNeighborCount()).isEqualTo(1);
         assertThat(grid.getCell(2, 1).getNeighborCount()).isEqualTo(1);
         assertThat(grid.getCell(2, 3).getNeighborCount()).isEqualTo(1);
-        assertThat(grid.getCell(3, 1).getNeighborCount()).isEqualTo(1);
         assertThat(grid.getCell(3, 2).getNeighborCount()).isEqualTo(1);
-        assertThat(grid.getCell(3, 3).getNeighborCount()).isEqualTo(1);
+
+        assertThat(grid.getCell(1, 1).getNeighborCount()).isEqualTo(0);
+        assertThat(grid.getCell(3, 3).getNeighborCount()).isEqualTo(0);
+        assertThat(grid.getCell(1, 3).getNeighborCount()).isEqualTo(0);
+        assertThat(grid.getCell(3, 1).getNeighborCount()).isEqualTo(0);
     }
 
     @Test
     public void notifyNeighborsBiggerRadius() {
-        neighborhood = new MooreNeighborhood(grid, 3);
+        neighborhood = new VonNeumannNeighborhood(grid, 3);
         Cell cell = new SimpleCell(5, 5, 1);
 
         grid.putCell(cellFactory.create(2, 5));
