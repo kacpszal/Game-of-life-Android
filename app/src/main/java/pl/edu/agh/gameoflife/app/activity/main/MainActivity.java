806746ba4047802261418d06e65e7cca734bd026
sienmich <michal.sienczak@gmail.com>
2018-03-27 01:13:33 +0200

    Tab first attempt

--- app/src/main/java/pl/edu/agh/gameoflife/app/activity/main/MainActivity.java
+++ app/src/main/java/pl/edu/agh/gameoflife/app/activity/main/MainActivity.java
@@ -1,12 +1,18 @@
 package pl.edu.agh.gameoflife.app.activity.main;
 
 import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.Fullscreen;
+import org.androidannotations.annotations.ViewById;
+
 import pl.edu.agh.gameoflife.R;
 
 
@@ -14,15 +20,20 @@ import pl.edu.agh.gameoflife.R;
 @Fullscreen
 public class MainActivity extends Activity {
 
+/*    @ViewById
+    RelativeLayout relative_layout_main;*/
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // TODO: Remove or change this placeholder text
-        /*TransitionDrawable transition =  (TransitionDrawable) activity_main.getBackground();
-        transition.isCrossFadeEnabled();
-        transition.startTransition(4500);*/
+        /*relative_layout_main.setBackgroundResource(R.drawable.background_animation);
+        AnimationDrawable animationDrawable =  (AnimationDrawable) relative_layout_main.getBackground();
+        animationDrawable.setEnterFadeDuration(4500);
+        animationDrawable.setExitFadeDuration(4500);
+        animationDrawable.start();*/
 
 
     }
