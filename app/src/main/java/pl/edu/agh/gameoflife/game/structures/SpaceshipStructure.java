4e32df5e2a00f531d93c654ef6c9dc5e6c80a69b
Kacper Szalwa <kacper511@gmail.com>
2018-03-31 17:29:52 +0200

    Structures refactoring.

--- app/src/main/java/pl/edu/agh/gameoflife/game/structures/SpaceshipStructure.java
+++ app/src/main/java/pl/edu/agh/gameoflife/game/structures/SpaceshipStructure.java
@@ -13,34 +13,34 @@ public class SpaceshipStructure extends AbstractStructure {
         listOfStructure = new ArrayList<>();
 
         if (x + 7 <= width && y + 14 <= height) {
-            listOfStructure.add(new SimpleCell(x + 1, y + 3, 1));
-            listOfStructure.add(new SimpleCell(x + 2, y + 3, 1));
-            listOfStructure.add(new SimpleCell(x + 5, y + 3, 1));
-            listOfStructure.add(new SimpleCell(x + 6, y + 3, 1));
-            listOfStructure.add(new SimpleCell(x + 3, y + 4, 1));
-            listOfStructure.add(new SimpleCell(x + 4, y + 4, 1));
-            listOfStructure.add(new SimpleCell(x + 3, y + 5, 1));
-            listOfStructure.add(new SimpleCell(x + 4, y + 5, 1));
-            listOfStructure.add(new SimpleCell(x, y + 6, 1));
-            listOfStructure.add(new SimpleCell(x + 2, y + 6, 1));
-            listOfStructure.add(new SimpleCell(x + 5, y + 6, 1));
-            listOfStructure.add(new SimpleCell(x + 7, y + 6, 1));
-            listOfStructure.add(new SimpleCell(x, y + 7, 1));
-            listOfStructure.add(new SimpleCell(x + 7, y + 7, 1));
-            listOfStructure.add(new SimpleCell(x, y + 9, 1));
-            listOfStructure.add(new SimpleCell(x + 7, y + 9, 1));
-            listOfStructure.add(new SimpleCell(x + 1, y + 10, 1));
-            listOfStructure.add(new SimpleCell(x + 2, y + 10, 1));
-            listOfStructure.add(new SimpleCell(x + 5, y + 10, 1));
-            listOfStructure.add(new SimpleCell(x + 6, y + 10, 1));
-            listOfStructure.add(new SimpleCell(x + 2, y + 11, 1));
-            listOfStructure.add(new SimpleCell(x + 3, y + 11, 1));
-            listOfStructure.add(new SimpleCell(x + 4, y + 11, 1));
-            listOfStructure.add(new SimpleCell(x + 5, y + 11, 1));
-            listOfStructure.add(new SimpleCell(x + 3, y + 13, 1));
-            listOfStructure.add(new SimpleCell(x + 4, y + 13, 1));
-            listOfStructure.add(new SimpleCell(x + 3, y + 14, 1));
-            listOfStructure.add(new SimpleCell(x + 4, y + 14, 1));
+            listOfStructure.add(new SimpleCell(x + 1, y + 3, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x + 2, y + 3, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x + 5, y + 3, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x + 6, y + 3, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x + 3, y + 4, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x + 4, y + 4, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x + 3, y + 5, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x + 4, y + 5, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x, y + 6, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x + 2, y + 6, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x + 5, y + 6, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x + 7, y + 6, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x, y + 7, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x + 7, y + 7, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x, y + 9, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x + 7, y + 9, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x + 1, y + 10, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x + 2, y + 10, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x + 5, y + 10, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x + 6, y + 10, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x + 2, y + 11, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x + 3, y + 11, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x + 4, y + 11, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x + 5, y + 11, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x + 3, y + 13, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x + 4, y + 13, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x + 3, y + 14, Cell.STATE_ALIVE));
+            listOfStructure.add(new SimpleCell(x + 4, y + 14, Cell.STATE_ALIVE));
         }
     }
 
